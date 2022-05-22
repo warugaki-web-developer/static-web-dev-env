e7ad55b44ba9ff3495166f1d7daa6fb0b0aceec1
warugaki <warugaki.web.developer@gmail.com>
2022-05-23 00:46:12 +0900

    WIP: Lint & フォーマットを導入

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': 'error',
+  },
+};
